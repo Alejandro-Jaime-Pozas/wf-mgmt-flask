ee263817092eefa92c84f17bf6191784d44b100a
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2022-12-12 18:02:10 -0600

    finished admin and employee creating other db models

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,6 @@
 from . import app
+from flask import render_template
 
+@app.route('/') # no methods here like GET or POST
+def index():
+    return render_template('index.html') # automatically looks to the templates folder in the app folder, no need to specify
